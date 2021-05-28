95f674099365a1311cfeefd07480658295fe67ff
sarahleah <>
2021-05-28 08:18:39 +0000

    Created function using loop

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function every(array, test) {
-  
+  let counter = 0;
+  for (let item of array) {
+    if (test(item) == true) {
+      counter++
+    }
+  }
+  if (counter == array.length) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 console.log(every([1, 3, 5], n => n < 10));
